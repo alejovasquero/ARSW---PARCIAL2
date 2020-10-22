077511afc8c568956cef9fcdcab33f0e30ddfea1
alejovasquero <alejo_704@hotmail.com>
2020-10-22 12:28:22 -0500

    Heroku

--- target/classes/static/js/buttonseeker.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(document).on('click', '.seeker', function() {
-        var $movie = $(this).closest(".omg").find("td[name='movie']").text();
-        var $fecha = $(this).closest(".omg").find("td[name='fecha']").text();
-        var $cinema = app.getCurrentCinema();
-        app.setFunctionByNameDateMovie($cinema, $fecha, $movie);
-    });
-});
